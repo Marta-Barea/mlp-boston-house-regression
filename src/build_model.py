e3a5e7a60f423c4646d74178bad51585249180e2
Marta Barea-Sepúlveda <79462299+Marta-Barea@users.noreply.github.com>
2025-06-11 17:14:52 +0200

    add weight initialization

--- src/build_model.py
+++ src/build_model.py
@@ -5,8 +5,9 @@ from tensorflow.keras.models import Sequential
 
 def build_model(input_dim, units=13, learning_rate=0.001):
     model = Sequential()
-    model.add(Dense(units, input_dim=input_dim, activation='relu'))
-    model.add(Dense(1))
+    model.add(Dense(units, input_dim=input_dim,
+              activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(1, activation='linear', kernel_initializer='glorot_uniform'))
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
                   loss='mean_squared_error',
                   metrics=['mean_absolute_error'])
